39596b31c4ec0646af1769251715a8e38d6b28a6
Kyle Novak <kyle@novak.codes>
2018-04-14 23:23:14 -0500

    change entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/Game.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
